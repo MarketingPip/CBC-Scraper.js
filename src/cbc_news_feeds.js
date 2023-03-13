ef7fed9e7184ae0f80181e1ff7aa2eb27b9f0b55
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2023-03-13 06:09:07 -0400

    Create cbc_news_feeds.js

---
+++ src/cbc_news_feeds.js
@@ -0,0 +1,32 @@
+{
+    "general": {
+        "topstories": "http://rss.cbc.ca/lineup/topstories.xml",
+        "world": "http://rss.cbc.ca/lineup/world.xml",
+        "canada": "http://rss.cbc.ca/lineup/canada.xml",
+        "politics": "http://rss.cbc.ca/lineup/politics.xml",
+        "business": "http://rss.cbc.ca/lineup/business.xml",
+        "health": "http://rss.cbc.ca/lineup/health.xml",
+        "arts": "http://rss.cbc.ca/lineup/arts.xml",
+        "technology": "http://rss.cbc.ca/lineup/technology.xml",
+        "offbeat": "http://rss.cbc.ca/lineup/offbeat.xml",
+        "aboriginal": "http://www.cbc.ca/cmlink/rss-cbcaboriginal"
+    },
+    "sports": {
+        "topstories": "http://rss.cbc.ca/lineup/sports.xml",
+        "mlb": "http://rss.cbc.ca/lineup/sports-mlb.xml",
+        "nba": "http://rss.cbc.ca/lineup/sports-nba.xml",
+        "curling": "http://rss.cbc.ca/lineup/sports-curling.xml",
+        "cfl": "http://rss.cbc.ca/lineup/sports-cfl.xml",
+        "nfl": "http://rss.cbc.ca/lineup/sports-nfl.xml",
+        "nhl": "http://rss.cbc.ca/lineup/sports-nhl.xml",
+        "soccer": "http://rss.cbc.ca/lineup/sports-soccer.xml",
+        "figureskating": "http://rss.cbc.ca/lineup/sports-figureskating.xml"
+    }
+}
+
+
+/// 
+
+[{"British Columbia":"https://rss.cbc.ca/lineup/canada-britishcolumbia.xml"},{"Kamloops":"https://rss.cbc.ca/lineup/canada-kamloops.xml"},{"Calgary":"https://rss.cbc.ca/lineup/canada-calgary.xml"},{"Edmonton":"https://rss.cbc.ca/lineup/canada-edmonton.xml"},{"Saskatchewan":"https://rss.cbc.ca/lineup/canada-saskatchewan.xml"},{"Saskatoon":"https://rss.cbc.ca/lineup/canada-saskatoon.xml"},{"Manitoba":"https://rss.cbc.ca/lineup/canada-manitoba.xml"},{"Thunder Bay":"https://rss.cbc.ca/lineup/canada-thunderbay.xml"},{"Sudbury":"https://rss.cbc.ca/lineup/canada-sudbury.xml"},{"Windsor":"https://rss.cbc.ca/lineup/canada-windsor.xml"},{"London":"https://www.cbc.ca/cmlink/rss-canada-london.xml"},{"Kitchener-Waterloo":"https://rss.cbc.ca/lineup/canada-kitchenerwaterloo.xml"},{"Toronto":"https://rss.cbc.ca/lineup/canada-toronto.xml"},{"Hamilton":"https://rss.cbc.ca/lineup/canada-hamiltonnews.xml"},{"undefined":"httpsundefined"},{"New Brunswick":"https://rss.cbc.ca/lineup/canada-newbrunswick.xml"},{"Prince Edward Island":"https://rss.cbc.ca/lineup/canada-pei.xml"},{"Nova Scotia":"https://rss.cbc.ca/lineup/canada-novascotia.xml"},{"Newfoundland & Labrador":"https://rss.cbc.ca/lineup/canada-newfoundland.xml"},{"North":"https://rss.cbc.ca/lineup/canada-north.xml"},{"Ottawa":"https://www.cbc.ca/cmlink/rss-canada-ottawa.xml"},{"undefined":"httpsundefined"}]
+/// 
+https://rss.cbc.ca/lineup/thenational.xml
